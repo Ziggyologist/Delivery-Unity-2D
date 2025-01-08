b4bef29b5f18fcabafb0a6a2bddfc12b7e685a2a
Teodora Vela <teodora11vela@gmail.com>
2025-01-08 18:28:51 +0200

    added assets and script for follow camera

--- Delivery-2D/Assets/Collision.cs
+++ Delivery-2D/Assets/Collision.cs
@@ -7,4 +7,14 @@ public class Collision : MonoBehaviour
     {
         Debug.Log("collision happened with " + collision.gameObject.name);
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("trigger happened with: " + collision.gameObject.name);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("exit the trigger with: " + collision.gameObject.name);   
+    }
 }
--- Delivery-2D/Assets/Driver.cs
+++ Delivery-2D/Assets/Driver.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 250f;
+    [SerializeField] float steerSpeed = 220f;
     [SerializeField] float moveSpeed = 10f;
 
     void Start()
