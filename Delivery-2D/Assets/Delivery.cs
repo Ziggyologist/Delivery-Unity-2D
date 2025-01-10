892de4c525a20489174e74aeaaa84c4a0ed633f4
Teodora Vela <teodora11vela@gmail.com>
2025-01-10 19:19:41 +0200

    added boost and slow zones

--- Delivery-2D/Assets/Delivery.cs
+++ Delivery-2D/Assets/Delivery.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
 
+
 public class Delivery : MonoBehaviour
 {
     [SerializeField] float destroyDelay = 0.5f;
+    SpriteRenderer carSprite;
     bool hasPackage = false;
+    Color pickedColor;
+    Color noPackageColor = Color.white;
 
+    private void Start()
+    {
+        carSprite = GetComponent<SpriteRenderer>();
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         //Debug.Log("collision happened with " + collision.gameObject.name);
@@ -12,10 +20,12 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag=="Package" && hasPackage == false)
+        if (collision.tag=="Package" && hasPackage == false)
         {
             Debug.Log("Package picked up!");
             hasPackage = true;
+            pickedColor = collision.GetComponent<SpriteRenderer>().color;
+            carSprite.color = pickedColor;
             Destroy(collision.gameObject, destroyDelay);
             return;
         }
@@ -29,6 +39,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package delivered!");
             hasPackage = false;
+            carSprite.color = noPackageColor;
             return;
         }
         else
--- Delivery-2D/Assets/Driver.cs
+++ Delivery-2D/Assets/Driver.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -6,10 +7,15 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 200f;
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float slowSpeed = 2f;
+    [SerializeField] float boostSpeed = 20f;
+    [SerializeField] float defaultSpeed = 10f;
+    [SerializeField] float buffDuration = 5f;
 
-    void Start()
-    {
-    }
+    bool isBoosted = false;
+    bool isSlowed = false;
+    float boostTimer = 0f;
+    Coroutine boostCoroutine;
 
     void Update()
     {
@@ -19,5 +25,79 @@ public class Driver : MonoBehaviour
         transform.Translate(0, moveAmount, 0);
     }
 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Boost"))
+        {
+            HandleBoost();
+        }
+    }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isBoosted)
+        {
+            StopCoroutine(boostCoroutine);
+            isBoosted = false;  // Reset boost state on collision
+        }
+        HandleSlow();
+    }
+
+    void HandleBoost()
+    {
+        if (!isBoosted)
+        {
+            boostCoroutine = StartCoroutine(BoostRoutine());
+        }
+        else
+        {
+            boostTimer += buffDuration;  // Extend boost duration
+        }
+    }
+
+    void HandleSlow()
+    {
+        isSlowed = true;
+        moveSpeed = slowSpeed;
+        Invoke(nameof(ResetSlow), buffDuration);
+    }
+
+    IEnumerator BoostRoutine()
+    {
+        isBoosted = true;
+        boostTimer = buffDuration;
+        moveSpeed = boostSpeed;
+
+        while (boostTimer > 0)
+        {
+            boostTimer -= Time.deltaTime;
+            yield return null;
+        }
+
+        isBoosted = false;
+        ReturnToSpeed();
+    }
+
+    void ResetSlow()
+    {
+        isSlowed = false;
+        ReturnToSpeed();
+    }
+
+    void ReturnToSpeed()
+    {
+        if (!isBoosted && !isSlowed)
+        {
+            moveSpeed = defaultSpeed;
+        }
+        else if (isBoosted)
+        {
+            moveSpeed = boostSpeed;
+        }
+        else if (isSlowed)
+        {
+            moveSpeed = slowSpeed;
+        }
+    }
 }
+
